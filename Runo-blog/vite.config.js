2afc1e33c120a726c082014c4baa3e26ff1ecefe
Folakl <yakubroimot2001@gmail.com>
2025-01-28 05:13:07 -0800

    Fix vite.config.js and rebuild

--- Runo-blog/vite.config.js
+++ Runo-blog/vite.config.js
@@ -1,8 +1,17 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   base: '/Runo-blog/',
+//   plugins: [react()],
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  base: '/Runo-blog/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/Runo-blog/' : '/', // Use '/' locally, '/Runo-blog/' for production
   plugins: [react()],
-})
+}))
+
